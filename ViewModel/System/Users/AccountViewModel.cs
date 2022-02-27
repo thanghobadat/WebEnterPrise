0552de3b2b8868934069024021107414f3b9ff73
Thanh Ngo <thanhngo.ha@outlook.com.vn>
2022-02-27 16:30:58 +0700

    add

---
+++ ViewModel/System/Users/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ViewModel.System.Users
+{
+    public class AccountViewModel
+    {
+        public Guid Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ViewModel/System/Users/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ViewModel.System.Users
+{
+    public class RegisterRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+    }
+}

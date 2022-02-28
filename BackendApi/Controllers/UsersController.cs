29d4758d74de25a21a082acd22f9c5ead4d40795
Thanh Ngo <thanhngo.ha@outlook.com.vn>
2022-02-28 20:53:43 +0700

    done sprint 1 api

--- BackendApi/Controllers/UsersController.cs
+++ BackendApi/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.System.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 using ViewModel.System.Users;
 
@@ -8,7 +9,7 @@ namespace BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -34,7 +35,54 @@ namespace BackendApi.Controllers
             }
             return Ok(resultToken);
         }
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userService.RegisterAccount(request);
+            if (!result.ResultObj)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
 
+        [HttpGet("GetAllAccount")]
+        public async Task<IActionResult> GetAllAccount([FromQuery] string keyword)
+        {
+            var result = await _userService.GetAllAccount(keyword);
+            return Ok(result);
+        }
 
+        [HttpGet("GetAccountById")]
+        public async Task<IActionResult> GetAccountById(Guid id)
+        {
+            var user = await _userService.GetAccountById(id);
+            return Ok(user);
+        }
+        [HttpPut("ChangePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.ChangePassword(request);
+            if (!result.IsSuccessed)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("DeleteAccount")]
+        public async Task<IActionResult> DeleteAccount(Guid id)
+        {
+            var result = await _userService.DeleteAccount(id);
+            return Ok(result);
+        }
     }
 }
--- ViewModel/System/Users/AccountViewModel.cs
+++ ViewModel/System/Users/AccountViewModel.cs
@@ -9,5 +9,7 @@ namespace ViewModel.System.Users
         public string UserName { get; set; }
         public string Email { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string Role { get; set; }
+        public string Department { get; set; }
     }
 }
